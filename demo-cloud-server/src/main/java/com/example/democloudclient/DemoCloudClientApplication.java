ea0c585eefbb6bc950384c3d2b92469caa0296e9
yangtianfeng <yangtianfeng@youxin.com>
2019-03-12 11:00:43 +0800

    "cloud start"

---
+++ demo-cloud-server/src/main/java/com/example/democloudclient/DemoCloudClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.democloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCloudClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCloudClientApplication.class, args);
+    }
+
+}
